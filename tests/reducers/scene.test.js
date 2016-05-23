61a6d97ad9e415f5ec574d8c4c311071b8341ffe
Wildhoney <adam.timberlake@gmail.com>
2016-05-23 11:29:11 +0100

    Added world reducer test

--- tests/reducers/scene.test.js
+++ tests/reducers/scene.test.js
@@ -10,6 +10,7 @@ test('It should return the initial state by default;', t => {
 test('It should be able to define the appartus entities;', t => {
     const modifiedState = { renderer: Symbol('a'), camera: Symbol('b'), scene: Symbol('c') };
     const state = sceneReducer(undefined, { type: SET_APPARATUS, ...modifiedState });
+    t.not(state, modifiedState);
     t.is(state.scene, modifiedState.scene);
     t.is(state.camera, modifiedState.camera);
     t.is(state.renderer, modifiedState.renderer);
