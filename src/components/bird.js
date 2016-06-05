c444f04976c78c2a63ed166fda1edc2847f6eba5
Wildhoney <adam.timberlake@gmail.com>
2016-06-05 15:06:47 +0100

    Started applying polygons

--- src/components/bird.js
+++ src/components/bird.js
@@ -9,7 +9,17 @@ export default class extends Component {
     render() {
 
         return (
-            <section className="bird" />
+            <section className="bird">
+                <div className="left-body"></div>
+                <div className="left-wing"></div>
+                <div className="top-left"></div>
+                <div className="lower-left"></div>
+                <div className="left-antenna"></div>
+                {/*<div className="beak-left"></div>*/}
+                <div className="foot-left"></div>
+                <div className="toe-left"></div>
+                <div className="toe-right"></div>
+            </section>
         );
 
 
