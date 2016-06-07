396394f4d12d1927933fff155ec47082e52ad5fe
Wildhoney <adam.timberlake@gmail.com>
2016-06-07 22:51:45 +0100

    Added BEM styling

--- src/components/bird.js
+++ src/components/bird.js
@@ -10,15 +10,30 @@ export default class extends Component {
 
         return (
             <section className="bird">
-                <div className="left-body"></div>
-                <div className="left-wing"></div>
-                <div className="top-left"></div>
-                <div className="lower-left"></div>
-                <div className="left-antenna"></div>
-                {/*<div className="beak-left"></div>*/}
-                <div className="foot-left"></div>
-                <div className="toe-left"></div>
-                <div className="toe-right"></div>
+
+                <div className="wing-left">
+                    <div className="wing-left__wing"></div>
+                    <div className="wing-left__feather wing-left__feather--first"></div>
+                    <div className="wing-left__feather wing-left__feather--second"></div>
+                    <div className="wing-left__feather wing-left__feather--third"></div>
+                </div>
+
+                <div className="hair">
+                    <div className="hair__tuft"></div>
+                </div>
+
+                <div className="body">
+                    <div className="body__left-ribs"></div>
+                    <div className="body__left-side"></div>
+                    <div className="body__breast"></div>
+                </div>
+
+                <div className="foot-left">
+                    <div className="foot-left__leg"></div>
+                    <div className="foot-left__toe-left"></div>
+                    <div className="foot-left__toe-right"></div>
+                </div>
+
             </section>
         );
 
