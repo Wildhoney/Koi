3c0e767ad60ffd005e9f923b8abd975a091805e0
Wildhoney <adam.timberlake@gmail.com>
2017-03-20 22:59:43 +0000

    Removed old src files

--- src/events.js
+++
@@ -1,2 +0,0 @@
-export const SET_DIMENSIONS = Symbol('set_dimensions');
-export const SET_APPARATUS = Symbol('set_apparatus');
--- src/reducers.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import scene from './reducers/scene';
-
-export default combineReducers({
-    scene
-});
